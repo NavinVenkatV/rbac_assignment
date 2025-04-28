91f764b95711b137a9b31574f10cb8dc45d70beb
NavinVenkatV <vnavinvenkat@gmail.com>
2025-04-28 07:35:28 +0530

    metadata

--- frontend/src/component/Form.tsx
+++ frontend/src/component/Form.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import axios from "axios"
-import Spinner from './ui/Spinner';
 
 
 function CreateBlogForm() {
@@ -11,38 +10,37 @@ function CreateBlogForm() {
     const [subtitle, setSubtitle] = useState('');
     const [content, setContent] = useState('');
     const [tags, setTags] = useState('');
-    const [loading, setLoading] = useState(true)
 
     const handleButton = async () => {
         console.log("Entered blog submit");
-
+    
         // Check for missing fields
         if (!mainImage || !title || !category || !subtitle || !content || !tags) {
             alert("Enter all the inputs, admin bro!");
             return;
         }
-
+    
         // Validate file type (if needed)
         const allowedTypes = ["image/jpeg", "image/png"];
         if (!allowedTypes.includes(mainImage.type)) {
             alert("Please upload a valid image (JPEG/PNG).");
             return;
         }
-
+    
         // Validate image size (e.g., max 5MB)
         const maxSize = 5 * 1024 * 1024; // 5MB
         if (mainImage.size > maxSize) {
             alert("File size exceeds the 5MB limit.");
             return;
         }
-
+    
         // Get token from localStorage
         const token = localStorage.getItem("token");
         if (!token) {
             alert("Token missing!");
             return;
         }
-
+    
         // Prepare form data
         const formData = new FormData();
         formData.append("mainImage", mainImage);
@@ -51,7 +49,7 @@ function CreateBlogForm() {
         formData.append("subtitle", subtitle);
         formData.append("content", content);
         formData.append("tags", tags);
-
+    
         try {
             // Send POST request to the backend
             const response = await axios.post('http://localhost:3001/createBlog', formData, {
@@ -60,7 +58,7 @@ function CreateBlogForm() {
                     token,  // Add token to the headers
                 },
             });
-
+    
             if (response.data) {
                 alert("Blog uploaded successfully!");
                 // Clear form inputs after successful upload
@@ -70,32 +68,31 @@ function CreateBlogForm() {
                 setSubtitle('');
                 setTags('');
                 setTitle('');
-                setLoading(false)
             }
         } catch (error) {
             console.error("Error:", error);
             alert("Error uploading blog!");
         }
     };
-
-
+    
+      
 
     return (
         <div className="w-full  md:px-32 mt-10 bg-neutral-800 text-white shadow-lg rounded-2xl md:p-8">
             <div className=' md:flex md:gap-36'>
-                <div className='py-10'>
-                    <h2 className="font-bold mb-6 text-2xl md:text-7xl text-center text-orange-700 ">Create New Blog</h2>
-                    <video
-                        className="mt-5 w-[500px] h-[600px] rounded-2xl object-cover"
-                        src="/vid.mp4"
-                        autoPlay
-                        muted
-                        loop
-                        playsInline
-                    ></video>
-                </div>
-                <div className='w-full md:w-[500px] mt-22 p-2 py-10'>
-                    {/* Upload Blog DP
+            <div className='py-10'>
+                <h2 className="font-bold mb-6 text-2xl md:text-7xl text-center text-orange-700 ">Create New Blog</h2>
+                <video
+                    className="mt-5 w-[500px] h-[600px] rounded-2xl object-cover"
+                    src="/vid.mp4"
+                    autoPlay
+                    muted
+                    loop
+                    playsInline
+                ></video>            
+            </div>
+            <div  className='w-full md:w-[500px] mt-22 p-2 py-10'>
+                {/* Upload Blog DP
                 <div className="mb-5">
                     <label className="block text-white text-2xl font-semibold mb-2">Upload Blog Display Image</label>
                     <input
@@ -107,87 +104,86 @@ function CreateBlogForm() {
                     />
                 </div> */}
 
-                    {/* Upload Main Image */}
-                    <div className="mb-5">
-                        <label className="block text-neutral-500 text-2xl font-semibold mb-2">Upload Main Blog Image</label>
-                        <input
-                            type="file"
-                            accept="image/*"
-                            onChange={(e) => setMainImage(e.target.files ? e.target.files[0] : null)}
-                            className="block w-full text-sm text-gray-500 border cursor-pointer file:mr-4 file:cursor-pointer rounded-full file:py-2 file:px-4 file:border-0 
+                {/* Upload Main Image */}
+                <div className="mb-5">
+                    <label className="block text-neutral-500 text-2xl font-semibold mb-2">Upload Main Blog Image</label>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        onChange={(e) => setMainImage(e.target.files ? e.target.files[0] : null)}
+                        className="block w-full text-sm text-gray-500 border cursor-pointer file:mr-4 file:cursor-pointer rounded-full file:py-2 file:px-4 file:border-0 
                      file:text-sm file:font-semibold transition-all duration-300 ease-in-out file:bg-white file:text-black "
-                        />
-                    </div>
-
-                    {/* Title */}
-                    <div className="mb-5">
-                        <label className="block text-neutral-500 text-2xl font-semibold mb-2">Blog Title</label>
-                        <input
-                            type="text"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                            placeholder="Enter the title..."
-                            className="w-full px-4 py-2 border border-neutral-700 focus:outline-none rounded-lg"
-                        />
-                    </div>
-
-                    <div className="mb-5">
-                        <label className="block text-neutral-500 text-2xl font-semibold mb-2">Blog Category</label>
-                        <input
-                            type="text"
-                            value={category}
-                            onChange={(e) => setCategory(e.target.value)}
-                            placeholder="Enter the title..."
-                            className="w-full px-4 py-2 border border-neutral-700 focus:outline-none rounded-lg"
-                        />
-                    </div>
-
-                    {/* Subtitle */}
-                    <div className="mb-5">
-                        <label className="block text-neutral-500 text-2xl font-semibold mb-2">Subtitle</label>
-                        <input
-                            type="text"
-                            value={subtitle}
-                            onChange={(e) => setSubtitle(e.target.value)}
-                            placeholder="Enter subtitle..."
-                            className="w-full px-4 py-2 border border-neutral-700 focus:outline-none rounded-lg"
-                        />
-                    </div>
-
-                    {/* Content */}
-                    <div className="mb-5">
-                        <label className="block text-neutral-500 text-2xl font-semibold mb-2">Content</label>
-                        <textarea
-                            value={content}
-                            onChange={(e) => setContent(e.target.value)}
-                            placeholder="Write your blog content..."
-                            rows={6}
-                            className="w-full px-4 py-2 border border-neutral-700 focus:outline-none rounded-lg"
-                        ></textarea>
-                    </div>
-
-                    {/* Tags */}
-                    <div className="mb-5">
-                        <label className="block text-neutral-500 text-2xl font-semibold mb-2">Tags</label>
-                        <input
-                            type="text"
-                            value={tags}
-                            onChange={(e) => setTags(e.target.value)}
-                            placeholder="eg. Tech, Coding, Life"
-                            className="w-full px-4 py-2 border border-neutral-700 focus:outline-none rounded-lg"
-                        />
-                    </div>
-
-                    <div className="flex justify-center">
-                        <button
-                            className="bg-white text-black hover:text-white cursor-pointer hover:bg-neutral-900 font-bold py-2 px-6 rounded-full transition-all duration-200 ease-in-out"
-                            onClick={handleButton}
-                        >
-                            {loading ? <Spinner />
-                                : "Create a Blog"}
-                        </button>
-                    </div>
+                    />
                 </div>
+
+                {/* Title */}
+                <div className="mb-5">
+                    <label className="block text-neutral-500 text-2xl font-semibold mb-2">Blog Title</label>
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder="Enter the title..."
+                        className="w-full px-4 py-2 border border-neutral-700 focus:outline-none rounded-lg"
+                    />
+                </div>
+
+                <div className="mb-5">
+                    <label className="block text-neutral-500 text-2xl font-semibold mb-2">Blog Category</label>
+                    <input
+                        type="text"
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                        placeholder="Enter the title..."
+                        className="w-full px-4 py-2 border border-neutral-700 focus:outline-none rounded-lg"
+                    />
+                </div>
+
+                {/* Subtitle */}
+                <div className="mb-5">
+                    <label className="block text-neutral-500 text-2xl font-semibold mb-2">Subtitle</label>
+                    <input
+                        type="text"
+                        value={subtitle}
+                        onChange={(e) => setSubtitle(e.target.value)}
+                        placeholder="Enter subtitle..."
+                        className="w-full px-4 py-2 border border-neutral-700 focus:outline-none rounded-lg"
+                    />
+                </div>
+
+                {/* Content */}
+                <div className="mb-5">
+                    <label className="block text-neutral-500 text-2xl font-semibold mb-2">Content</label>
+                    <textarea
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        placeholder="Write your blog content..."
+                        rows={6}
+                        className="w-full px-4 py-2 border border-neutral-700 focus:outline-none rounded-lg"
+                    ></textarea>
+                </div>
+
+                {/* Tags */}
+                <div className="mb-5">
+                    <label className="block text-neutral-500 text-2xl font-semibold mb-2">Tags</label>
+                    <input
+                        type="text"
+                        value={tags}
+                        onChange={(e) => setTags(e.target.value)}
+                        placeholder="eg. Tech, Coding, Life"
+                        className="w-full px-4 py-2 border border-neutral-700 focus:outline-none rounded-lg"
+                    />
+                </div>
+
+                <div className="flex justify-center">
+                    <button
+                        className="bg-white text-black hover:text-white cursor-pointer hover:bg-neutral-900 font-bold py-2 px-6 rounded-full transition-all duration-200 ease-in-out"
+                        onClick={handleButton}
+                    >
+                        Create Blog
+                    </button>
+                </div>
+            </div>
             </div>
         </div>
     );
